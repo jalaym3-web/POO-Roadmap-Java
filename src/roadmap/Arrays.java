1f88234aada2302ba4579c7099c70e91ede40295
Jose Alay <jalaym3@miumg.edu.gt>
2026-06-01 18:34:39 -0600

    Arrays

---
+++ src/roadmap/Arrays.java
@@ -0,0 +1,13 @@
+package roadmap;
+
+public class Arrays {
+	
+	int[] calificaciones1 = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
+	{
+
+	for(int i=0; i < calificaciones1.length; i++) {
+	    System.out.println( calificaciones1[i] );
+	}
+	
+}
+}
